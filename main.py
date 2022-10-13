d4577c2839c64bcb38f3b3b84ea5c74698daee15
vileluv <s.kolupov@icloud.com>
2022-10-13 18:11:14 +0300

    v1.0

---
+++ main.py
@@ -0,0 +1,6 @@
+with open ("lec003_ex1.txt", "wt", encoding="cp1251") as f:
+    print('абвгд\nеёжэи', file=f)
+    print('клмн', file=f)
+with open ('lec003_ex1.txt', 'rb')as f_rb:
+    content = f_rb.read(10)
+print([v for v in content[4:8]])
