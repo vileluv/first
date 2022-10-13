52290c8031d6bfb2e255fcfa56d7203663ada6a6
vileluv <s.kolupov@icloud.com>
2022-10-13 20:53:37 +0300

    V1.1

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ with open ("lec003_ex1.txt", "wt", encoding="cp1251") as f:
 with open ('lec003_ex1.txt', 'rb')as f_rb:
     content = f_rb.read(10)
 print([v for v in content[4:8]])
+print("amogus")
